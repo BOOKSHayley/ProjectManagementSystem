1e0e52e5b49625bb1805fcf33cab5ef26decbabc
Logan-Lafauci <78313635+Logan-Lafauci@users.noreply.github.com>
2022-11-08 10:42:25 -0600

    finishing task page and updating modal to match

--- src/views/viewTaskPage.js
+++ src/views/viewTaskPage.js
@@ -2,7 +2,11 @@ var ViewTaskPage = Backbone.View.extend({
     events: {
         'click #sendComment': 'updateComments',
         'keyup #comment': 'enterComments',
-        'click #openAssignModal': 'openAssignModal'
+        'click #openAssignModal': 'openAssignModal',
+        'keyup #assignUsers-selectUser' : 'enterAssigned',
+        'click #assignUserButton': 'updateAssigned',
+        'click #deleteUserButton': 'deleteUser',
+        'click #confirmUsers': 'confirmNewUsers',
     },
 
     initialize: function(){
@@ -33,9 +37,67 @@ var ViewTaskPage = Backbone.View.extend({
         $("#comment").val("");
     },
 
+    updateAssigned: function(){
+        var newUser = $("#assignUsers-selectUser").val();
+        var users = this.model.get("users");
+        var addNewUser;
+        var index;
+        
+        for(let i =0; i < users.length; i++)
+        {
+            if(newUser == users[i].name)
+            {
+                addNewUser = users[i];
+                index = i;
+                break;
+            }
+        }
+
+        if(newUser != null){
+            var tableEntry = '<tr class="assignUserRow" data-index="'+index+'"><td>'+addNewUser.name+'</td><td><button type="button" class="btn" id="deleteUserButton"><span class="material-icons" style="color: red">delete</span></button></td></tr>';
+            $("#newAssignedUsers").append(tableEntry);
+        }
+        $("#assignUsers-selectUser").val("");
+    },
+
+    deleteUser: function(){
+        $(event.target).closest("tr").remove();
+    },
+
+    confirmNewUsers: function(){
+        var assigned = this.model.get("assigned");
+        var users = this.model.get("users");
+        var notAssigned = true;
+        $(".assignUserRow").each(function(){
+            for(let i = 0; i < assigned.length; i++)
+            {
+                if(assigned[i] == users[tmp])
+                {
+                    notAssigned=false;
+                }
+            }
+            
+            if(notAssigned)
+            {
+                var tmp = $(this).attr("data-index");
+                assigned.push(users[tmp]);
+            }
+        });
+        this.model.set("assigned", assigned);
+        $("#assignModal").modal("hide");
+        $(".modal-backdrop").remove();
+        this.render();
+    },
+
     enterComments: function(e){
         if(e.keyCode == 13){
             this.updateComments();
+        }
+    },
+
+    enterAssigned: function(e){
+        if(e.keyCode == 13){
+            this.updateAssigned();
         }
     },
 
